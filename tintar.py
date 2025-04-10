4315816e45006dce0aa4a19932e899c266ce1c9c
Mihai Danaila <slendyman35@gmail.com>
2025-04-10 16:49:32 +0300

    heuristics

--- tintar.py
+++ tintar.py
@@ -76,8 +76,8 @@ class state:
                     eatenTable = tabla.copy()
                     eatenTable[i] = ''
                     generated.append(state(eatenTable,
-                                            self.pieseAlbastre if self.turn == 'x' else self.pieseAlbastre + 1,
-                                            self.pieseRosii if self.turn == '0' else self.pieseRosii + 1,
+                                            self.pieseAlbastre,
+                                            self.pieseRosii,
                                             self,
                                             '0' if self.turn == 'x' else 'x'))
                     
@@ -131,33 +131,148 @@ class state:
                 if newTable[position] != self.turn:
                     continue
                 
-                for connection in self.connections[position]:
-                    if newTable[connection] != '':
-                        continue
-                    
-                    newMoveTable = newTable.copy()
-                    newMoveTable[connection] = self.turn
-                    newMoveTable[position] = ''
-                    
-                    # print(f"am mutat de la {position} la {connection}")
-                    
-                    #Verificam daca am facut moara
-                    eatenFlag = False
-                    
-                    if self._verificareMoara(connection, newMoveTable):
-                        eatenBoards = self._manancaPiese(newMoveTable)
-                        generated += eatenBoards
-                        eatenFlag = True
-                        
-                    if not eatenFlag:
-                        generated.append(state(newMoveTable,
-                                                self.pieseAlbastre,
-                                                self.pieseRosii,
-                                                self,
-                                                '0' if self.turn == 'x' else 'x'))
+                pieseTabla = sum([1 for i in range(len(newTable)) if newTable[i] == self.turn])
+                
+                if pieseTabla > 3:
+                    #Putem muta doar pe pozitiile adiacente
+                    generated += self._movePiece(self.connections[position], newTable, position)
+                else:
+                    #putem muta oriunde
+                    generated += self._movePiece(range(len(self.tabla)), newTable, position)
+
                 
                 
         return generated
+
+    def _movePiece(self, loopRange, newTable, oldPosition):
+        generated = []
+        
+        for newPosition in loopRange:
+            if newTable[newPosition] != '':
+                continue
+            
+            newMoveTable = newTable.copy()
+            newMoveTable[newPosition] = self.turn
+            newMoveTable[oldPosition] = ''
+            
+            # print(f"am mutat de la {position} la {newPosition}")
+            
+            #Verificam daca am facut moara
+            eatenFlag = False
+            
+            if self._verificareMoara(newPosition, newMoveTable):
+                eatenBoards = self._manancaPiese(newMoveTable)
+                generated += eatenBoards
+                eatenFlag = True
+                
+            if not eatenFlag:
+                generated.append(state(newMoveTable,
+                                        self.pieseAlbastre,
+                                        self.pieseRosii,
+                                        self,
+                                        '0' if self.turn == 'x' else 'x'))
+        
+        return generated
+    
+    def gameOver(self):
+        # Verificam daca mai avem mutari disponibile sau daca mai are piese pe tabla
+        piese = 0
+        for position in range(len(self.tabla)):
+            if self.tabla[position] == self.turn:
+                piese += 1
+                for connection in self.connections[position]:
+                    if self.tabla[connection] == '':
+                        return False
+        
+        pieseDePus = self.pieseAlbastre if self.turn == 'x' else self.pieseRosii
+        if piese == 0 and pieseDePus == 0:
+            return True
+        
+        return True
+    
+    def _morrisNumber(self):
+        return sum(1 for moara in self.mori if all(self.tabla[i] == self.turn for i in moara))
+    
+    def _blockedOpps(self):
+        opponent = "0" if self.turn == "x" else "x"
+        blocked_opponent_pieces = sum(
+            1
+            for i in range(len(self.tabla))
+            if self.tabla[i] == opponent
+            and all(self.tabla[neighbor] != "" for neighbor in self.connections[i])
+        )
+        
+        return blocked_opponent_pieces
+    
+    def _piecesNumber(self):
+        return sum([1 for i in range(len(self.tabla)) if self.tabla[i] == self.turn])
+    
+    def _twoPiecesConfig(self):
+        return sum(
+            1
+            for moara in self.mori
+            if sum(self.tabla[i] == self.turn for i in moara) == 2
+            and any(self.tabla[i] == "" for i in moara)
+        )
+        
+    def _threePiecesConfig(self):
+        return sum(
+            1 for moara in self.mori if all(self.tabla[i] == self.turn for i in moara)
+        )
+        
+    def _openMorris(self):
+
+        return sum(
+            1 for moara in self.mori
+            if sum(self.tabla[i] == self.turn for i in moara) == 2 and any(self.tabla[i] == '' for i in moara)
+        )
+        
+    def _doubleMorris(self):
+        double_morris_count = 0
+
+        for position in range(len(self.tabla)):
+            if self.tabla[position] == self.turn:
+                morrises_with_position = [
+                    moara for moara in self.mori if position in moara
+                ]
+
+                formed_morrises = sum(
+                    1 for moara in morrises_with_position if all(self.tabla[i] == self.turn for i in moara)
+                )
+
+                if formed_morrises >= 2:
+                    double_morris_count += 1
+
+        return double_morris_count
+    
+    def _winning(self):
+        pass #TODO
+    
+    def eval(self, moaraFormata = False):
+        C1,C2,C3,C4,C5,C6,C7 = None, None, None, None, None, None, None
+        phase = None
+        
+        if(self.pieseAlbastre + self.pieseRosii != 0):
+            C1,C2,C3,C4,C5,C6 = (18, 26, 1, 6, 12, 7)
+            phase = 1
+            
+            return C1 * moaraFormata + C2 * self._morrisNumber() + C3 * self._blockedOpps() + C4 * self._piecesNumber() + C5 * self._twoPiecesConfig() + C6 * self._threePiecesConfig()
+            
+        elif sum([1 for i in range(len(self.tabla)) if self.tabla[i] == self.turn]) > 3:
+            C1,C2,C3,C4,C5,C6,C7 = (14, 43, 10, 8, 7, 42, 1086)
+            phase = 2
+            
+            return C1 * moaraFormata + C2 * self._morrisNumber() + C3 * self._blockedOpps() + C4 * self._piecesNumber() + C5 * self._openMorris() + C6 * self._doubleMorris() + C7 * self._winning()
+        else:
+            C1,C2,C3,C4 = (10, 1, 16, 1190)
+            phase = 3
+            
+        score = 0
+        
+    
+        
+    
+        return score
         
     def __str__(self):
         t = self.tabla  # for convenience
@@ -182,6 +297,23 @@ class state:
     
     def __repr__(self):
         return f"{self.tabla}"
+    
+class minmax:
+    def __init__(self, startNode: state, adancime: int, alpha: int, beta: int):
+        self.nodCurent = startNode
+        self.adancime = adancime
+        self.alpha = alpha
+        self.beta = beta
+        
+    def __str__(self):
+        return f"Adancime: {self.adancime}, Alpha: {self.alpha}, Beta: {self.beta}"
+    
+    def __repr__(self):
+        return f"{self.nodCurent}"
+    
+    def minmax(self, position, adancime):
+        if adancime == 0 or position.gameOver():
+            return position.eval()
     
 state1 = state(['','','','','','x','','','0','','x','x','0','x','','','','0','x','0','x','','0',''], 3, 4 )
 
